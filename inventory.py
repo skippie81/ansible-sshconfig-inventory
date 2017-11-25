2adfa4e6bc96105be6433aa27fa7eab410a806fb
skippie <joeri@scapicchio.be>
2017-11-25 21:22:27 +0100

    initial commit

--- inventory.py
+++ inventory.py
@@ -3,6 +3,7 @@
 import argparse
 import ConfigParser
 import os
+import json
 from inventory import sshConfigInventory
 
 settings = {
@@ -12,6 +13,8 @@ settings = {
         'ignore_hosts': []
     }
 }
+
+ssh_inventory = sshConfigInventory()
 
 def configure():
     config = ConfigParser.ConfigParser()
@@ -33,23 +36,20 @@ def configure():
                 settings[groupname][name] = value
     except ConfigParser.Error:
         return False
-    print settings
     return True
 
 # main part of the program
-def main(style='json'):
-    ssh_inventory = sshConfigInventory()
+def read():
     ssh_inventory.read(settings['ssh_config']['ssh_config_file'],settings['ssh_config']['ignore_hosts'])
     for group in ssh_inventory.groups():
         if group in settings.keys():
             for name in settings[group].keys():
                 ssh_inventory.add_group_var(group,name,settings[group][name])
-    print ssh_inventory.get_inventory()
 
 if __name__ == '__main__':
     # ceate the argument parser
     parser = argparse.ArgumentParser(description='Dynamic ansible inventory with ssh_config as source')
-    parser.add_argument('-l','--list',action='store_true',help='output ')
+    parser.add_argument('-l','--list',action='store_true',help='output inventory json format')
     parser.add_argument('--host',type=str,nargs=1,help='get hostvars of a host')
     parser.add_argument('-H',action='store_true',help='Print human readable ini style invenotry')
     parser.add_argument('--version',action='version',version='%(prog)s 0.0.1-beta1')
@@ -57,6 +57,19 @@ if __name__ == '__main__':
     # parse the arguments
     args = parser.parse_args()
 
+    style = 'json'
+    if args.list:
+        style='json'
+    if args.H:
+        style='ini'
+
     # read configuration file
     configure()
-    main()
+    read()
+
+    # output
+    if args.host != None:
+        hostvars = ssh_inventory.hostvars(args.host[0])
+        print json.dumps(hostvars,sort_keys=True, indent=4, separators=(',', ': '))
+    else:
+        print ssh_inventory.get_inventory(style)
